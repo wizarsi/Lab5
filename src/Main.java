c1447e6a16db0c055d92fb380cee2f59293e06bf
unknown <filiston431@gmail.com>
2021-03-04 15:11:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+/**
+ * @author Andrey Vasilev
+ * Главный класс, через который программа начинает работу
+ */
+public class Main {
+    public static void main(String[] args) throws IOException{
+        CollectionManager collectionManager = new CollectionManager();
+        Commander commander = new Commander(collectionManager);
+        //collectionManager.parse();
+        commander.run();
+    }
+}
